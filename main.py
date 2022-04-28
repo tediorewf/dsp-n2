6bfb5e58df464d337ae68e5e2bea0a202509b938
Mikhail Eremeev <meremeev@sfedu.ru>
2022-04-28 12:19:19 +0300

    feat: add 7-13 calculations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from cmath import pi
+from cmath import pi, cos
 import os
 import operator
 import random
@@ -15,8 +15,13 @@ TASK_DIR = os.path.join(BASE_DIR, 'task/')
 SA_FNAME = os.path.join(TASK_DIR, 'Sa4.tx')
 
 REPORT_DIR = os.path.join(BASE_DIR, 'report/')
-IMG0_FNAME = os.path.join(REPORT_DIR, '0.png')
 IMG1_FNAME = os.path.join(REPORT_DIR, '1.png')
+IMG2_FNAME = os.path.join(REPORT_DIR, '2.png')
+IMG3_FNAME = os.path.join(REPORT_DIR, '3.png')
+IMG4_FNAME = os.path.join(REPORT_DIR, '4.png')
+IMG5_FNAME = os.path.join(REPORT_DIR, '5.png')
+IMG6_FNAME = os.path.join(REPORT_DIR, '6.png')
+IMG7_FNAME = os.path.join(REPORT_DIR, '7.png')
 
 
 def parse_file(f):
@@ -34,26 +39,51 @@ def save_plot(xs, ys, img_fname):
     plt.clf()
 
 
-EDGES = (-3.0, 3.0)
+def generate_random_signal(amplitude, duration):
+    signal = [
+        random.uniform(
+            -amplitude, amplitude
+        ) for _ in range(duration)
+    ]
+    return signal
+
+
+# Амплитуда
+A = 0.5
+# Длительность сигнала
 N = 512
 
 
-def generate_random_signal():
-    signal = [
-        random.uniform(*EDGES) for _ in range(N)
+def compute_energy(xt):
+    energy = sum((x*x for x in xt))
+    return energy
+
+
+def xt_signal(amplitude, freq1, freq2, dt, k):
+    if freq1 >= freq2:
+        raise ValueError('Error: freq1 >= freq2')
+    
+    result = amplitude*cos(freq1*k*dt) + 2*amplitude*cos(freq2*k*dt)
+    return result
+
+
+def make_discrete_xt(
+    amplitude, freq1, freq2, dt, ks_count
+):
+    discrete_xt = [
+        xt_signal(
+            amplitude, freq1, freq2, dt, k
+        ) for k in range(ks_count)
     ]
-    return signal
+    return discrete_xt
 
 
 def process_file(f):
     # Содержимое файла с вариантом
     dt, xt = parse_file(f)
-    
+
     # Отсчёты
     ks = list(range(N))
-
-    # Сохраняем исходный сигнал
-    save_plot(ks, xt, IMG0_FNAME)
 
     # Дискретное преобразование Фурье (512 комплексных чисел)
     complexs = np.fft.fft(xt)
@@ -91,11 +121,101 @@ def process_file(f):
         )
     )
 
+    # Картинка 2
+    noise1 = generate_random_signal(A + 19.5, N)
+    noisy_signal1 = list(map(operator.add, xt, noise1))
+    complexs_noisy_signal1 = np.fft.fft(noisy_signal1)
+    moduls_noisy_signal1 = list(map(sp.Abs, complexs_noisy_signal1))
+    save_plot(ks, moduls_noisy_signal1, IMG2_FNAME)
+
     # Генерируем случайный сигнал (шум)
-    noise = generate_random_signal()
+    noise2 = generate_random_signal(A, N)
 
     # Исходный сигнал с шумом
-    noisy_signal = list(map(operator.add, xt, noise))
+    noisy_signal2 = list(map(operator.add, xt, noise2))
+
+    # Дискретное преобразование фурье зашумлённого сигнала
+    complexs_noisy_signal2 = np.fft.fft(noisy_signal2)
+
+    # Модули комплексных чисел в том же порядке
+    # (сигнал с шумом)
+    moduls_noisy_signal2 = list(map(sp.Abs, complexs_noisy_signal2))
+
+    # Картинка 3
+    save_plot(ks, moduls_noisy_signal2, IMG3_FNAME)
+
+    energy_base_signal = compute_energy(xt)
+    energy_noise2 = compute_energy(noise2)
+    print(
+        'Энергия исходного сигнала: {}\nЭнергия шума: {}'.format(
+            energy_base_signal, energy_noise2
+        )
+    )
+
+    # Картинка 4
+    discrete_xt1 = make_discrete_xt(
+        amplitude=0.7,
+        freq1=100,
+        freq2=245,
+        dt=dt,
+        ks_count=N,
+    )
+    base_with_discrete1 = list(map(operator.add, xt, discrete_xt1))
+    complexs_base_with_discrete_fft1 = np.fft.fft(base_with_discrete1)
+    moduls_base_with_discrete1 = list(map(sp.Abs, complexs_base_with_discrete_fft1))
+    save_plot(ks, moduls_base_with_discrete1, IMG4_FNAME)
+
+    # Картинка 5
+    discrete_xt2 = make_discrete_xt(
+        amplitude=0.7,
+        freq1=100,
+        freq2=415,
+        dt=dt,
+        ks_count=N,
+    )
+    base_with_discrete2 = list(map(operator.add, xt, discrete_xt2))
+    complexs_base_with_discrete_fft2 = np.fft.fft(base_with_discrete2)
+    moduls_base_with_discrete2 = list(map(sp.Abs, complexs_base_with_discrete_fft2))
+    save_plot(ks, moduls_base_with_discrete2, IMG5_FNAME)
+
+    # Картинка 6
+    amplitude = 0.7
+    freq1 = 100
+    freq2 = 124
+
+    discrete_xt3 = make_discrete_xt(
+        amplitude=amplitude,
+        freq1=freq1,
+        freq2=freq2,
+        dt=dt,
+        ks_count=N,
+    )
+    base_with_discrete3 = list(map(operator.add, xt, discrete_xt3))
+    complexs_base_with_discrete_fft3 = np.fft.fft(base_with_discrete3)
+    moduls_base_with_discrete3 = list(map(sp.Abs, complexs_base_with_discrete_fft3))
+    save_plot(ks, moduls_base_with_discrete3, IMG6_FNAME)
+    
+    # Берём чётные отсчёты
+    _k = 0
+    ks_even = []
+    while _k < N//2:
+        ks_even.append(_k)
+        _k += 2
+    dt_x2 = 2*dt
+
+    xt_evens = [xt[k] for k in ks_even]
+
+    short_discrete_xt3 = make_discrete_xt(
+        amplitude=amplitude,
+        freq1=freq1,
+        freq2=freq2,
+        dt=dt_x2,
+        ks_count=N//2,
+    )
+    short_base_with_discrete3 = list(map(operator.add, xt_evens, short_discrete_xt3))
+    short_complexs_base_with_discrete_fft3 = np.fft.fft(short_base_with_discrete3)
+    short_moduls_base_with_discrete3 = list(map(sp.Abs, short_complexs_base_with_discrete_fft3))
+    save_plot(ks_even, short_moduls_base_with_discrete3, IMG7_FNAME)
 
 
 with open(SA_FNAME, 'r') as f:
